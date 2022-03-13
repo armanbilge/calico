441675f8c78256a9ee16ea0f3ca3eb0aca1761b5
Arman Bilge <armanbilge@gmail.com>
2022-03-13 17:38:52 +0000

    WIP Rx

--- calico/src/main/scala/calico/Rx.scala
+++ calico/src/main/scala/calico/Rx.scala
@@ -17,37 +17,54 @@
 package calico
 
 import cats.Monad
+import cats.effect.IO
+import cats.effect.kernel.Concurrent
 import cats.effect.kernel.Ref
 import cats.effect.kernel.Resource
 import cats.effect.kernel.Sync
 import cats.effect.kernel.Unique
 import cats.syntax.all.*
+import cats.~>
+import fs2.Pull
+import fs2.Stream
 
-trait Rx[F[_], +A]:
-  // def sources: Set[Unique.Token]
+trait Reactive[F[_]]:
 
+  def rx[A](a: A): F[Rx[F, A]]
+
+  def stream[A](sa: Stream[F, A]): Resource[F, RxSource[F, A]]
+
+object Reactive:
+  given Reactive[IO] with
+    def rx[A](a: A): IO[Rx[IO, A]] = ???
+
+    def stream[A](sa: Stream[IO, A]): Resource[IO, RxSource[IO, A]] =
+      sa.pull.uncons1.flatMap(Pull.output1).stream.unNone.compile.resource.lastOrError.flatMap {
+        (a, tail) =>
+          Resource.eval(rx(a)).flatTap { rx => tail.foreach(rx.set).compile.resource.drain }
+      }
+
+trait RxSource[F[_], A]:
+  def map[B](f: A => B): Resource[F, RxSource[F, B]]
   def foreach(f: Rx.Foreach[A]): Resource[F, Unit]
-
-  def map[B](f: A => B): Resource[F, Rx[F, B]]
-
-  def map2[B, C](that: Rx[F, B])(f: (A, B) => C): Resource[F, Rx[F, C]]
 
 trait RxSink[F[_], A]:
   def set(a: A): F[Unit]
 
-private abstract class AbstractRxRef[F[_], A](
-    val sources: Set[Unique.Token],
-    listeners: Ref[F, Set[A => F[Unit]]]
-)(using F: Monad[F])
-    extends Rx[F, A]
-    with RxSink[F, A]:
+trait Rx[F[_], A] extends RxSource[F, A], RxSink[F, A]
 
-  def get: F[A] = ???
+private final class RxRef[F[_], A](
+    value: Ref[F, A],
+    listeners: Ref[F, Set[Rx.Foreach[A]]]
+)(using F: Sync[F])
+    extends Rx[F, A]:
 
-  def set(a: A): F[Unit] = ???
+  def set(a: A): F[Unit] = value.set(a)
 
-  def foreach(f: A => F[Unit]): Resource[F, Unit] =
-    Resource.make(listeners.update(_ + f) *> (get >>= f))(_ => listeners.update(_ - f))
+  def foreach(f: Rx.Foreach[A]): Resource[F, Unit] =
+    Resource.make {
+      listeners.update(_ + f) *> (value.get.flatMap(f[F](_)))
+    } { _ => listeners.update(_ - f) }
 
   def map[B](f: A => B): Resource[F, Rx[F, B]] = ???
 
