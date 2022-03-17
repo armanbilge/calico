57f7efe607c9a25f7dc3b73b52d7102779015dfe
Arman Bilge <armanbilge@gmail.com>
2022-03-17 17:32:57 +0000

    Refactor the DSL

--- calico/src/main/scala/calico/dsl.scala
+++ calico/src/main/scala/calico/dsl.scala
@@ -70,72 +70,84 @@ trait HtmlBuilders[F[_]](using F: Async[F])
   protected def prop[V, J](name: String, codec: Codec[V, J]) =
     Prop(name, codec)
 
-  def children: HtmlChildren[F] = HtmlChildren[F]
-
 type HtmlTagT[F[_]] = [E <: dom.HTMLElement] =>> HtmlTag[F, E]
 final class HtmlTag[F[_], E <: dom.HTMLElement] private[calico] (name: String, void: Boolean)(
     using F: Async[F]):
-  // def apply[EE >: E](modifiers: Modifier[F, EE]*): Resource[F, E] =
-  //   build.toResource.flatTap { e => modifiers.traverse_(_.modify(e)) }
 
-  // def apply(text: Stream[Rx[F, *], String]): Resource[F, E] =
-  //   build.toResource.flatTap { e =>
-  //     text.foreach(t => Rx(e.innerText = t)).compile.drain.background.mapK(Rx.renderK)
-  //   }
+  def apply[M](modifier: M)(using Modifier[F, E, M]): Resource[F, E] =
+    apply(Tuple1(modifier))
 
-  def apply[M <: Tuple](mods: M)(
-      using inst: K0.ProductInstances[Mod[F, E, *], M]): Resource[F, E] =
-    inst.foldLeft(mods)(build.toResource) {
-      [a] => (r: Resource[F, E], m: Mod[F, E, a], a: a) => r.flatTap(m.modify(a, _))
+  def apply[M <: Tuple](modifiers: M)(
+      using inst: K0.ProductInstances[Modifier[F, E, *], M]): Resource[F, E] =
+    inst.foldLeft(modifiers)(build.toResource) {
+      [a] => (r: Resource[F, E], m: Modifier[F, E, a], a: a) => r.flatTap(m.modify(a, _))
     }
 
   private def build = F.delay(dom.document.createElement(name).asInstanceOf[E])
 
-trait Mod[F[_], E, A]:
+trait Modifier[F[_], E, A]:
   def modify(a: A, e: E): Resource[F, Unit]
 
-object Mod:
-  given [F[_], E <: dom.HTMLElement](using F: Sync[F]): Mod[F, E, String] with
-    def modify(s: String, e: E): Resource[F, Unit] = F.delay(e.innerText = s).toResource
+object Modifier:
+  given [F[_], E <: dom.Element](using F: Sync[F]): Modifier[F, E, String] with
+    def modify(s: String, e: E): Resource[F, Unit] = F.delay(e.innerText += s).toResource
 
-sealed trait Modifier[F[_], E]:
-  def modify(e: E): Resource[F, Unit]
-
-final class HtmlChildren[F[_]] private[calico] (using F: Async[F]):
-  def :=(children: List[Resource[F, dom.HTMLElement]]): Modifier[F, dom.HTMLElement] =
-    new:
-      def modify(e: dom.HTMLElement) =
-        children.sequence.flatMap(_.traverse_(c => F.delay(e.appendChild(c)).toResource))
+  given [F[_], E <: dom.Element, E2 <: dom.Element](
+      using F: Sync[F]): Modifier[F, E, Resource[F, E2]] with
+    def modify(e2: Resource[F, E2], e: E): Resource[F, Unit] =
+      e2.evalMap(e2 => F.delay(e.appendChild(e2)))
 
 final class HtmlAttr[F[_]: Async, V] private[calico] (key: String, codec: Codec[V, String]):
-
-  def :=(v: V): Modifier[F, dom.HTMLElement] =
+  def :=(v: V): HtmlAttr.Modified[F, V] =
     this <-- Stream.emit(v)
 
-  def <--(vs: Stream[Rx[F, *], V]): Modifier[F, dom.HTMLElement] =
-    new:
-      def modify(e: dom.HTMLElement) =
-        vs.foreach { v => Rx(e.setAttribute(key, codec.encode(v))) }
-          .compile
-          .drain
-          .background
-          .void
-          .mapK(Rx.renderK)
+  def <--(vs: Stream[Rx[F, *], V]): HtmlAttr.Modified[F, V] =
+    HtmlAttr.Modified(key, codec, vs)
+
+object HtmlAttr:
+  final class Modified[F[_], V] private[calico] (
+      val key: String,
+      val codec: Codec[V, String],
+      val values: Stream[Rx[F, *], V]
+  )
+
+  given [F[_]: Async, E <: dom.Element, V]: Modifier[F, E, Modified[F, V]] with
+    def modify(attr: Modified[F, V], e: E) =
+      attr
+        .values
+        .foreach(v => Rx(e.setAttribute(attr.key, attr.codec.encode(v))))
+        .compile
+        .drain
+        .background
+        .void
+        .mapK(Rx.renderK)
 
 final class Prop[F[_]: Async, V, J] private[calico] (name: String, codec: Codec[V, J]):
-
-  def :=(v: V): Modifier[F, dom.HTMLElement] =
+  def :=(v: V): Prop.Modified[F, V, J] =
     this <-- Stream.emit(v)
 
-  def <--(vs: Stream[Rx[F, *], V]): Modifier[F, dom.HTMLElement] =
-    new:
-      def modify(e: dom.HTMLElement) =
-        vs.foreach { v =>
+  def <--(vs: Stream[Rx[F, *], V]): Prop.Modified[F, V, J] =
+    Prop.Modified(name, codec, vs)
+
+object Prop:
+  final class Modified[F[_], V, J](
+      val name: String,
+      val codec: Codec[V, J],
+      val values: Stream[Rx[F, *], V]
+  )
+
+  given [F[_]: Async, E <: dom.Element, V, J]: Modifier[F, E, Modified[F, V, J]] with
+    def modify(prop: Modified[F, V, J], e: E) =
+      prop
+        .values
+        .foreach { v =>
           Rx {
-            e.asInstanceOf[js.Dynamic].updateDynamic(name)(codec.encode(v).asInstanceOf[js.Any])
+            e.asInstanceOf[js.Dynamic]
+              .updateDynamic(prop.name)(prop.codec.encode(v).asInstanceOf[js.Any])
           }
-        }.compile
-          .drain
-          .background
-          .void
-          .mapK(Rx.renderK)
+        }
+        .compile
+        .drain
+        .background
+        .void
+        .mapK(Rx.renderK)
