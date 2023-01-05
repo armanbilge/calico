6ec50e50271ad2c50a1e5be7ccfb1f1032299055
Matthias Herrmann <matthias.mh.herrmann@gmail.com>
2023-01-05 14:11:00 +0100

    Header for generated files

--- calico/src/main/scala/calico/html/defs/props/HtmlProps.scala
+++ calico/src/main/scala/calico/html/defs/props/HtmlProps.scala
@@ -1,7 +1,24 @@
+/*
+ * Copyright 2022 Arman Bilge
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package calico.html.defs.props
 
 import calico.html.keys.HtmlProp
-import calico.html.codecs.Codec.*; import calico.html.codecs.AsIsCodec.*; import calico.html.codecs.*
+import calico.html.codecs.Codec.*
+import calico.html.codecs.AsIsCodec.*
+import calico.html.codecs.*
 
 // #NOTE: GENERATED CODE
 //  - This file is generated at compile time from the data in Scala DOM Types
--- project/src/main/scala/calico/html/codegen/CalicoGenerator.scala
+++ project/src/main/scala/calico/html/codegen/CalicoGenerator.scala
@@ -51,7 +51,11 @@ private[codegen] object CalicoGenerator
   override val baseScalaJsHtmlElementType: String = "HtmlElement[F]"
 
   override val codecsImport: String =
-    s"import ${basePackagePath}.codecs.Codec.*; import ${basePackagePath}.codecs.AsIsCodec.*; import ${basePackagePath}.codecs.*"
+    List(
+      s"import ${basePackagePath}.codecs.Codec.*",
+      s"import ${basePackagePath}.codecs.AsIsCodec.*",
+      s"import ${basePackagePath}.codecs.*"
+    ).mkString("\n")
 
   override def generateTagsTrait(
       tagType: TagType,
@@ -251,5 +255,25 @@ private[codegen] object CalicoGenerator
       outputImplDefs = true,
       format = format
     ).printTrait().getOutput()
+  }
+
+  override def writeToFile(packagePath: String, fileName: String, fileContent: String): Unit = {
+    val header =
+      """/*
+        | * Copyright 2022 Arman Bilge
+        | *
+        | * Licensed under the Apache License, Version 2.0 (the "License");
+        | * you may not use this file except in compliance with the License.
+        | * You may obtain a copy of the License at
+        | *
+        | *     http://www.apache.org/licenses/LICENSE-2.0
+        | *
+        | * Unless required by applicable law or agreed to in writing, software
+        | * distributed under the License is distributed on an "AS IS" BASIS,
+        | * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+        | * See the License for the specific language governing permissions and
+        | * limitations under the License.
+        | */""".stripMargin('|') + System.lineSeparator
+    super.writeToFile(packagePath, fileName, header + fileContent)
   }
 }
