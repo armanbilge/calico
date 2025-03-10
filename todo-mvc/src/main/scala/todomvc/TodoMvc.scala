b795b64d9ed101d39b918ac36d74b80406d05569
ansh7432 <anshsoni743@gmail.com>
2025-03-10 14:21:21 +0530

    fix scala

--- todo-mvc/src/main/scala/todomvc/TodoMvc.scala
+++ todo-mvc/src/main/scala/todomvc/TodoMvc.scala
@@ -19,7 +19,6 @@ package todomvc
 import calico.*
 import calico.frp.{*, given}
 import calico.html.io.{*, given}
-// import calico.html.nodes
 import calico.router.*
 import cats.data.*
 import cats.effect.*
